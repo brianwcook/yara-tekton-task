bc19b5551480f48a75eaf5440156ac8f9a75f542
Brian Cook <bcook@redhat.com>
2023-09-30 19:46:39 -0400

    wip

--- test.sh
+++ test.sh
@@ -1,11 +1,13 @@
 kubectl delete pipelineRun yara-scan-pipeline
 kubectl delete pipeline yara-scan-pipeline
+kubectl delete tr yara-scan-pipeline-yara-task
 kubectl delete tr yara-scan-pipeline-yara-scan
 kubectl delete task yara-task
+tkn taskrun delete yara-scan-pipeline-yara-task
+sleep 1
 kubectl apply -f 0.1/yara-task.yaml
 kubectl apply -f yara-pipeline.yaml
-
-# kubectl apply -f yara-pipeline-run.yaml
+kubectl apply -f yara-pipeline-run.yaml
 
 echo "use"
 echo "\ntkn pipelinerun logs yara-scan-pipeline -f\n"
